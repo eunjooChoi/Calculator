4e473c2c4c9c77b5b143cafc1d12c1669c6de16f
USER <user@al01142342.local>
2018-07-08 01:51:55 +0900

    ADD CONSTANT OPRATIONS WITH MVC

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -20,14 +20,26 @@
     func setoperand(operand: Double){
         accumulator = operand
     }
+    
+    var operations: Dictionary <String, Double> = [ "PIE": Double.pi, "e": M_E]
+    
     func performOperation(symbol: String){
-        switch symbol {
-        case "PIE":
-            accumulator = Double.pi
-        case "√":
-            accumulator = sqrt(accumulator)
-        default:
-            break
+        
+        // 이렇게 구현하면 switch문에 너무 많은 조건이 들어가게 되고 그러면 많이 지저분해진다
+        // 그래서 위에 딕셔너리 형으로 상수연산자들을 지정해두고 상수 연산자 버튼이 눌렸을 때 위에 저장한 함수를 실행하도록 만들었다
+        //        switch symbol {
+        //        case "PIE":
+        //            accumulator = Double.pi
+        //        case "√":
+        //            accumulator = sqrt(accumulator)
+        //        default:
+        //            break
+        //        }
+        
+        
+        // 그런데 딕셔너리에 symbol을 키로 갖는 값이 없을 수도 있으므로 반환형이 옵셔널이다
+        if let constant = operations[symbol] {
+            accumulator = constant
         }
     }
     
